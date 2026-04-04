a38abd04463a8456d1ac199db83726d79a94befa
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-04-03 21:51:49 -0400

    feat: add more robust queryset to Business model view

--- backend/businesses/views.py
+++ backend/businesses/views.py
@@ -9,5 +9,14 @@ class BusinessViewSet(viewsets.ModelViewSet):
     API endpoint that allows businesses to be viewed or edited.
     """
 
-    queryset = Business.objects.all()
     serializer_class = BusinessSerializers
+
+    def get_queryset(self):
+        user = self.request.user
+
+        if user.is_superuser:
+            return Business.objects.all().order_by("-created_at")
+
+        return Business.objects.filter(
+            id=user.business_id
+        ).order_by("-start_date")
