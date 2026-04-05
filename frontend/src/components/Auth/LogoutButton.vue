3273ca6e5bc9ad916a006b5efaed262e167ff2f6
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-04-05 12:24:47 -0400

    feat: added LogoutButton and LogoutView component. Render LogoutButton on DashboardView

---
+++ frontend/src/components/Auth/LogoutButton.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import ButtonItem from '@/components/ui/ButtonItem.vue'
+
+import { useAuthStore } from '@/stores/auth'
+import router from '@/router'
+
+const auth = useAuthStore()
+
+const handleLogout = async () => {
+	await auth.logout()
+	router.push('/logout')
+}
+</script>
+
+<template>
+	<ButtonItem @click="handleLogout" text="Cerrar sesión" />
+</template>
--- frontend/src/pages/DashboardView.vue
+++ frontend/src/pages/DashboardView.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { useAuthStore } from '@/stores/auth'
+import LogoutButton from '@/components/Auth/LogoutButton.vue'
+
 const store = useAuthStore()
 </script>
 
 <template>
-	<h1>Dashboard</h1>
-	<p>User first name: {{ store.user?.first_name }}</p>
+	<section class="flex flex-col gap-4 justify-center items-center">
+		<h1>Dashboard</h1>
+		<p>User first name: {{ store.user?.first_name }}</p>
+		<LogoutButton />
+	</section>
 </template>
