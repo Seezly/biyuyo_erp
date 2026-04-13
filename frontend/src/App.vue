6aa56a733ffc3243a814f581a36d13477e577918
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-04-13 19:42:22 -0400

    chore: reorganize layout components and update index.html styles. TheFooter added to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
-import TheHeader from './components/ui/TheHeader.vue'
+import TheHeader from './components/layout/TheHeader.vue'
+import TheFooter from './components/layout/TheFooter.vue'
 </script>
 
 <template>
 	<TheHeader />
-	<main class="max-w-7xl mt-8 flex-1 mx-auto w-full flex items-center justify-start">
+	<main class="max-w-7xl flex-1 mx-auto w-full flex items-center justify-start">
 		<RouterView />
 	</main>
+	<TheFooter />
 </template>
 
 <style scoped></style>
---
+++ frontend/src/components/layout/TheFooter.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+	<footer class="bg-primary w-full px-8 py-4">
+		<p class="text-white text-center">&copy; 2026 Biyuyo ERP. Todos los derechos reservados.</p>
+	</footer>
+</template>
