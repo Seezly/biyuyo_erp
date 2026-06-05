1d65934b581a9add5bd2c94a4f419a565f601da5
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-06-05 19:26:38 -0400

    feat: add skip-to-content link for keyboard navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,9 +6,19 @@ import Breadcrumb from './components/Breadcrumb.vue'
 </script>
 
 <template>
+	<a
+		href="#main-content"
+		class="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 focus:z-[10000] focus:bg-primary focus:text-white focus:px-4 focus:py-2 focus:rounded-lg focus:outline-none focus:ring-2 focus:ring-white"
+	>
+		Saltar al contenido principal
+	</a>
 	<Toast />
 	<TheHeader />
-	<main class="max-w-7xl flex-1 mx-auto w-full flex flex-col items-start justify-start gap-4">
+	<main
+		id="main-content"
+		tabindex="-1"
+		class="max-w-7xl flex-1 mx-auto w-full flex flex-col items-start justify-start gap-4"
+	>
 		<Breadcrumb />
 		<RouterView />
 	</main>
