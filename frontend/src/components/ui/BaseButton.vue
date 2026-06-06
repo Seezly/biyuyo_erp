1b56125cc78694ad1a04bc469ca903d99b08abf7
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2026-06-06 19:53:21 -0400

    feat(basebutton): add loading prop with spinner and disabled state

--- frontend/src/components/ui/BaseButton.vue
+++ frontend/src/components/ui/BaseButton.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-
 import { RouterLink } from 'vue-router'
 
 interface ButtonProps {
@@ -10,10 +9,11 @@ interface ButtonProps {
   type?: 'button' | 'submit' | 'reset'
   width?: 'full' | 'auto'
   to?: string
+  loading?: boolean
+  disabled?: boolean
 }
 
 const props = defineProps<ButtonProps>()
-
 const emit = defineEmits(['click'])
 
 const colors = {
@@ -32,28 +32,41 @@ const widths = {
   auto: 'w-auto',
 }
 
+const isDisabled = computed(() => props.disabled || props.loading)
+
 const buttonClasses = computed(() => [
   colors[props.variant || 'primary'],
   widths[props.width || 'full'],
   'py-2 px-4 rounded-full cursor-pointer',
+  isDisabled.value ? 'opacity-60 cursor-not-allowed' : '',
 ])
 
 const types = props.type || 'button'
 
 function onClick() {
-  emit('click')
+  if (!isDisabled.value) {
+    emit('click')
+  }
 }
 </script>
 
 <template>
-  <RouterLink v-if="to" :to="to" :class="widths[props.width || 'full']">
-    <button :type="types" :class="buttonClasses" @click="onClick">
+  <RouterLink v-if="to && !isDisabled" :to="to" :class="widths[props.width || 'full']">
+    <button :type="types" :class="buttonClasses" :disabled="isDisabled" :aria-busy="loading || undefined">
+      <svg v-if="loading" class="inline-block animate-spin mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" aria-hidden="true">
+        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+      </svg>
       {{ text }}
-      <i :class="icon"></i>
+      <i v-if="!loading" :class="icon"></i>
     </button>
   </RouterLink>
-  <button v-else :type="types" :class="buttonClasses" @click="onClick">
+  <button v-else :type="types" :class="buttonClasses" :disabled="isDisabled" :aria-busy="loading || undefined" @click="onClick">
+    <svg v-if="loading" class="inline-block animate-spin mr-2 h-4 w-4" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" aria-hidden="true">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+    </svg>
     {{ text }}
-    <i :class="icon"></i>
+    <i v-if="!loading" :class="icon"></i>
   </button>
 </template>
